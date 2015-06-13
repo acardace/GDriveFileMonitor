655c8bfaf5b40f451d630413aa3e9b3dc95f51df
Antonio Cardace <ichigo663@gmail.com>
2015-06-13 18:11:56 +0200

    added link to copy

--- main.c
+++ main.c
@@ -8,8 +8,14 @@
 #include "notifier.h"
 #include "uploader.h"
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(int argc, char *argv[])
 {
+   char *code_url = (char *) malloc( sizeof(char) * URL_LEN );
+   code_url = obtain_auth_code_url( code_url );
+
+   printf("Copy this link and paste it into your browser:\n%s\n",code_url);
+
    return 0;
 }
