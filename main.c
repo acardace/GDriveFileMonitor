c96f7b4bf32755110004ddaf3773f18a5ed9c7fa
Antonio Cardace <ichigo663@gmail.com>
2015-06-15 11:49:14 +0200

    token gets cached in a file

--- main.c
+++ main.c
@@ -6,29 +6,43 @@
  */
 
 #include "notifier.h"
-#include "gdriveUploader.h"
+#include "googleOAuth.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
 
 int main(int argc, char *argv[])
 {
    json_struct *json_data = (json_struct *) malloc( sizeof(json_struct) * JSON_RESPONSE_SIZE );
-   char *url = (char *) malloc( sizeof(char) * URL_LEN );
-   char *code = (char *) malloc( sizeof(char) * URL_LEN );
+   char *json_file_path = (char *) malloc( sizeof(char) * 128 );
 
-   url = obtain_auth_code_url( url );
+   json_file_path = strcpy( json_file_path, getenv("HOME") );
+   json_file_path = strcat( json_file_path, JSON_FILENAME );
 
-   printf("Copy this link and paste it into your browser:\n%s\n\n",url);
-   printf("Paste the code you obtained:\n");
+   /*check if already have an access token*/
+   if( access(json_file_path, R_OK) == -1 ){
+      char *url = (char *) malloc( sizeof(char) * URL_LEN );
+      char *code = (char *) malloc( sizeof(char) * URL_LEN );
 
-   scanf("%s",code);
+      url = obtain_auth_code_url( url );
 
-   if( !(json_data = exchange_code_for_token( code, json_data) ) ){
-      exit(1);
+      printf("Copy this link and paste it into your browser:\n%s\n\n",url);
+      printf("Paste the code you obtained:\n");
+
+      scanf("%s",code);
+
+      if( !(json_data = exchange_code_for_token( code, json_data, json_file_path) ) )
+         exit(1);
+
+      free(url);
+      free(code);
+   }else{
+      if( ! ( json_data = get_access_token(json_data, json_file_path) ) )
+         exit(1);
    }
 
-   free(url);
-   free(code);
+   free(json_file_path);
    free(json_data);
    return 0;
 }
